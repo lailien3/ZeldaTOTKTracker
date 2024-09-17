c7441f1dbc31ee3e482119fe303db921beee4607
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-09-17 17:25:16 +0100

    Changed heading names

--- src/LoadingPage.test.js
+++ src/LoadingPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import LoadingPage from './LoadingPage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<LoadingPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
